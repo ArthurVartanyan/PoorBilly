cfec1b4422d4b65684c1d1a9a2ecb0c54e1e7c8d
Rogue06 <rogue.2019@icloud.com>
2021-06-30 20:42:38 +0300

    Add Spring boot in project and tune configurations

--- src/main/java/com/poor/billy/PoorBilly.java
+++ src/main/java/com/poor/billy/PoorBilly.java
@@ -1,4 +1,11 @@
 package com.poor.billy;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PoorBilly {
+    public static void main(String[] args) {
+        SpringApplication.run(PoorBilly.class, args);
+    }
 }
