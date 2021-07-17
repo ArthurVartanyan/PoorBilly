4e1a20d796c26a584b59b50b53775181fd6ae85f
Rogue06 <rogue.2019@icloud.com>
2021-07-17 20:12:22 +0300

    Fix problem with Jenkins and Allure: can not find allure command line <null>

--- src/test/java/com/poor/billy/controller/UserControllerTest.java
+++ src/test/java/com/poor/billy/controller/UserControllerTest.java
@@ -1,27 +1,21 @@
 package com.poor.billy.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.poor.billy.dto.IncomeDTO;
 import com.poor.billy.dto.UserRegistrationDTO;
-import com.poor.billy.model.operation.IncomeType;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 // (
 //        SpringBootTest.WebEnvironment.MOCK,
