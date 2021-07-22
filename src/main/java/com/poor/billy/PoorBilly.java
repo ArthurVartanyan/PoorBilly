c4239705c8cf51174dd770f1b8450afbbf25ece2
Rogue06 <rogue.2019@icloud.com>
2021-07-22 18:55:47 +0300

    Add Swagger UI

--- src/main/java/com/poor/billy/PoorBilly.java
+++ src/main/java/com/poor/billy/PoorBilly.java
@@ -2,8 +2,9 @@ package com.poor.billy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class PoorBilly {
     public static void main(String[] args) {
